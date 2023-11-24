27884e1931ea1d48a6f80f283768d52153348540
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-11-24 18:11:27 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/hlo_papa")
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/lover_jerry")
         ],
     ]
     return buttons
